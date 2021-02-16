2264df104a0596906b2fc50d124deebe761b20d0
secminhr <secminhrian@gmail.com>
2021-02-16 19:56:07 +0800

    Initial Commit

---
+++ Cavern-iOS/Cavern_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cavern_iOSApp.swift
+//  Cavern-iOS
+//
+//  Created by secminhr on 2021/2/16.
+//
+
+import SwiftUI
+
+@main
+struct Cavern_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cavern-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cavern-iOS
+//
+//  Created by secminhr on 2021/2/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
